818e9e2f9750587b8cb0750ee4842d5334d5c7ee
vladover1 <anonim.rab@icloud.com>
2023-02-10 17:42:36 +0200

    useState

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PizzaBlock = ({title, price}) => {
+
+    const [pizzaCount, setPizzaCount] = useState(0)
+
+    const onClickAdd = () => {
+        setPizzaCount( pizzaCount => pizzaCount + 1)
+    }
+
     return (
         <div className="pizza-block">
             <img
@@ -22,7 +29,7 @@ const PizzaBlock = ({title, price}) => {
             </div>
             <div className="pizza-block__bottom">
                 <div className="pizza-block__price">от {price} ₴</div>
-                <div className="button button--outline button--add">
+                <button onClick={onClickAdd} className="button button--outline button--add">
                     <svg
                         width="12"
                         height="12"
@@ -36,8 +43,8 @@ const PizzaBlock = ({title, price}) => {
                         />
                     </svg>
                     <span>Добавить</span>
-                    <i>2</i>
-                </div>
+                    <i>{pizzaCount}</i>
+                </button>
             </div>
         </div>
     );
